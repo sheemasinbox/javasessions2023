a6602e947273f0d75891f3e27cbf87e30b0fc70b
Sheema Farheen <sheemasinbox@gmail.com>
2024-05-31 15:39:38 +0530

    updated OrderPage.java

--- src/Assignments/OrderPage.java
+++ src/Assignments/OrderPage.java
@@ -5,8 +5,9 @@ public class OrderPage {
 	public static void main(String[] args) {
 		
 		String str = "This is Order Page";
+		String str1 = "This is Order Page1";
 		
-		System.out.println(str);
+		System.out.println(str+str1);
 
 	}
 
